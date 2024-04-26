86dc8c6427db2cf48697ac37781690b53194a83a
scqilin <sql1211@qq.com>
2024-04-26 18:01:23 +0800

    add

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname,'preload.js')
+      }
     })
   
     win.loadFile('index.html')
   }
 
 // 等待应用准备就绪后创建窗口
-app.whenReady().then(createWindow)
+app.whenReady().then(()=>{
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createWindow()
+        }
+    })
+})
 
 // 关闭所有窗口时退出
 app.on('window-all-closed', () => {
